bcd9b0039edde63101cbc6055b12b87e03e20663
wzboun15 <59983003+wzboun15@users.noreply.github.com>
2020-01-19 19:55:28 -0500

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+# Project Name: Hello World
+# Author:Alwatheq Bellah Zboun
+# Date: 1/19/2020
+# declare variables
+Name = 'Alwatheq Bellah Zboun'
+Born ='Bethlehem'
+Major ='Information Systems'
+Age ='21'
+print('Hello World my name is '    +  Name)
+print ('I was born in '  +  Born)
+print ('I major in '  + Major)
+print( 'I am '  +  Age)
